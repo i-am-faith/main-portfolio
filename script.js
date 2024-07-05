b0a4f3187481564681576ec0a60db27660cb5c31
Sourin Mukherjee <sourin.mukherjee2580@gmail.com>
2024-07-05 14:50:52 +0530

    Responsive

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-
-var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Bussiness Developer", "Blogger"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
-
 function toggleNavbar() {
     var navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 }
 
-
-// code by sourin mukherjee
+var typed = new Typed(".typing-text", {
+    strings: ["Frontend Developer", "Competitive Programmer", "CPP Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
